55627e8669065d65c2269358fd2d96d60335d460
Глеб Попов <0504051@mail.ru>
2025-01-21 11:14:40 +0300

    swagger fulltime

--- ApiEasier.Server/Program.cs
+++ ApiEasier.Server/Program.cs
@@ -35,11 +35,8 @@ namespace ApiEasier.Server
             app.UseStaticFiles();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
