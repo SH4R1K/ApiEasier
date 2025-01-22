687fd10e53222fc3bd10dc184a0d93cc481b5843
Pluhenciya <vanya.ponomarv.00@bk.ru>
2025-01-22 10:23:08 +0300

    add xml documentation

---
+++ ApiEasier.Server/Db/MongoDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiEasier.Server.Db
+{
+    /// <summary>
+    /// Контекст для работы с MongoDB.
+    /// </summary>
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="MongoDBContext"/>.
+        /// </summary>
+        /// <param name="settings">Настройки базы данных, содержащие строку подключения и имя базы данных.</param>
+        public MongoDBContext(IOptions<DBSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        /// <summary>
+        /// Получает коллекцию по указанному имени.
+        /// </summary>
+        /// <typeparam name="T">Тип документов в коллекции.</typeparam>
+        /// <param name="name">Имя коллекции.</param>
+        /// <returns>Коллекция документов указанного типа.</returns>
+        public IMongoCollection<T> GetCollection<T>(string name) =>
+            _database.GetCollection<T>(name);
+
+        /// <summary>
+        /// Асинхронно получает список имен коллекций в базе данных.
+        /// </summary>
+        /// <returns>Список имен коллекций.</returns>
+        public async Task<List<string>> ListCollectionNamesAsync()
+        {
+            var collections = await _database.ListCollectionNamesAsync();
+            return await collections.ToListAsync();
+        }
+    }
+}
