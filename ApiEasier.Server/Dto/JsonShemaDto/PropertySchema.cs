af0ccef8eadd38fab366534c16d21b59a4a803bc
Pluhenciya <vanya.ponomarv.00@bk.ru>
2025-01-30 16:36:03 +0300

    horror json schema

---
+++ ApiEasier.Server/Dto/JsonShemaDto/PropertySchema.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiEasier.Server.Dto.JsonShemaDto
+{
+    public class PropertySchema
+    {
+        [JsonPropertyName("type")]
+        public object Type { get; set; } // Can be a string or an array of strings
+
+        [JsonPropertyName("format")]
+        public string Format { get; set; }
+
+        [JsonPropertyName("maximum")]
+        public int? Maximum { get; set; }
+
+        [JsonPropertyName("minimum")]
+        public int? Minimum { get; set; }
+
+        [JsonPropertyName("oneOf")]
+        public List<OneOfSchema> OneOf { get; set; }
+
+        [JsonPropertyName("items")]
+        public ReferenceOrPropertySchema Items { get; set; }
+
+        [JsonPropertyName("$ref")]
+        public string Ref { get; set; }
+
+        [JsonPropertyName("enum")]
+        public List<object> Enum { get; set; }
+
+        [JsonPropertyName("x-enumNames")]
+        public List<string> XEnumNames { get; set; }
+
+        [JsonPropertyName("additionalProperties")]
+        public bool? AdditionalProperties { get; set; }
+
+        [JsonPropertyName("properties")]
+        public Dictionary<string, PropertySchema> Properties { get; set; }
+    }
+}
---
+++ ApiEasier.Server/Dto/JsonShemaDto/ReferenceOrPropertySchema.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiEasier.Server.Dto.JsonShemaDto
+{
+    public class ReferenceOrPropertySchema
+    {
+        [JsonPropertyName("$ref")]
+        public string Ref { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
