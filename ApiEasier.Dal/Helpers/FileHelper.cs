088e672a5801332a7872e143ace9b78b1266b3eb
Yakov <yakovx95@mail.ru>
2025-02-04 16:04:56 +0300

    added Helpers!!!

---
+++ ApiEasier.Dal/Helpers/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace ApiEasier.Dal.Helpers
+{
+    public class FileHelper
+    {
+        private readonly string _folderPath;
+
+        public FileHelper(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+
+        public string GetFilePath(string fileName)
+        {
+            return Path.Combine(_folderPath, fileName + ".json");
+        }
+
+        public async Task<string?> ReadFileAsync(string fileName)
+        {
+            var filePath = GetFilePath(fileName);
+            return File.Exists(filePath) ? await File.ReadAllTextAsync(filePath) : null;
+        }
+
+        public async Task WriteFileAsync(string fileName, string content)
+        {
+            var filePath = GetFilePath(fileName);
+            await File.WriteAllTextAsync(filePath, content);
+        }
+    }
+}
---
+++ ApiEasier.Dal/Helpers/JsonHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ApiEasier.Dal.Helpers
+{
+    public class JsonHelper
+    {
+        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+
+        public static T? Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, Options);
+        }
+
+        public static string Serialize<T>(T? obj)
+        {
+            return JsonSerializer.Serialize(obj, Options);
+        }
+    }
+}
