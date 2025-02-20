a8cafc3cd049f91ce3a2d99b47ccb52b8bc299b0
Yakov <yakovx95@mail.ru>
2025-02-20 12:35:22 +0300

    OMAGAAA TEST?!?!?

--- tests/ApiEasier.Tests/Integration/Tests/ApiServiceTest.cs
+++ tests/ApiEasier.Tests/Integration/Tests/ApiServiceTest.cs
@@ -1,9 +1,9 @@
 using ApiEasier.Api;
 using ApiEasier.Bll.Dto;
 using ApiEasier.Dm.Models;
+using ApiEasier.Dm.Models.JsonShema;
 using ApiEasier.Tests.Integration.Base;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit.Abstractions;
@@ -39,37 +39,68 @@ namespace ApiEasier.Tests.Integration.Tests
                     {
                         Name = "TestApiEntity",
                         IsActive = true,
-                        Structure = null,
+                        Structure = new Dm.Models.JsonShema.JsonSchema
+                        {
+                            Schema = "http://json-schema.org/draft-07/schema#",
+                            Type = "object",
+                            Required = [ "name", "age", "email" ],
+                            Properties = new Dictionary<string, PropertySchema>
+                            {
+                                { "name", new PropertySchema { Type = "string" } },
+                                { "age", new PropertySchema { Type = "integer", Minimum = 18 } },
+                                { "email", new PropertySchema { Type = "string", Format = "email" } }
+                            }
+                        },
                         Endpoints = new List<ApiEndpointDto>
                         {
-                            new ApiEndpointDto { Route = "get", Type = TypeResponse.Get, IsActive = true },
-                            new ApiEndpointDto { Route = "post", Type = TypeResponse.Post, IsActive = true }
+                            new() { Route = "get", Type = TypeResponse.Get, IsActive = true },
+                            new() { Route = "post", Type = TypeResponse.Post, IsActive = true },
+                            new() { Route = "put", Type = TypeResponse.Put, IsActive = false },
+                            new() { Route = "delete", Type = TypeResponse.Delete, IsActive = false }
                         }
                     },
                     new ApiEntityDto
                     {
-                        Name = "TestApiEntityFulEndpoints",
-                        IsActive = true,
-                        Structure = null,
-                        Endpoints = new List<ApiEndpointDto>
-                        {
-                            new ApiEndpointDto { Route = "get", Type = TypeResponse.Get, IsActive = true },
-                            new ApiEndpointDto { Route = "post", Type = TypeResponse.Post, IsActive = true },
-                            new ApiEndpointDto { Route = "put", Type = TypeResponse.Put, IsActive = true },
-                            new ApiEndpointDto { Route = "delete", Type = TypeResponse.Delete, IsActive = true }
-                        }
+                        Name = "TestEmptyApiEntity",
+                        IsActive = false,
+                        Structure = null
                     },
                 }
             };
 
             var createResponse = await _client.PostAsJsonAsync("/api/ApiService", newApiService);
-
-            var responseBody = await createResponse.Content.ReadAsStringAsync();
-
-
-            _output.WriteLine($"Response: {responseBody}");
-
             createResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var location = createResponse.Headers.Location?.ToString();
+            location.Should().NotBeNull();
+
+            var getResponse = await _client.GetAsync(location);
+            getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var apiService = await getResponse.Content.ReadFromJsonAsync<ApiServiceDto>();
+
+            apiService.Should().NotBeNull();
+            apiService.Name.Should().Be("TestApiService");
+            apiService.IsActive.Should().BeTrue();
+            apiService.Description.Should().Be("TestDescription");
+            apiService.Entities.Should().HaveCount(2);
+
+            apiService.Entities[0].Name.Should().Be("TestApiEntity");
+            apiService.Entities[0].IsActive.Should().BeTrue();
+            apiService.Entities[0].Endpoints.Should().HaveCount(4);
+            apiService.Entities[0].Structure.Should().NotBeNull();
+            apiService.Entities[0].Endpoints[0].Route.Should().Be("get");
+            apiService.Entities[0].Endpoints[0].Type.Should().Be(TypeResponse.Get);
+            apiService.Entities[0].Endpoints[0].IsActive.Should().BeTrue();
+            apiService.Entities[0].Endpoints[3].Route.Should().Be("delete");
+            apiService.Entities[0].Endpoints[3].Type.Should().Be(TypeResponse.Delete);
+            apiService.Entities[0].Endpoints[3].IsActive.Should().BeFalse();
+
+            apiService.Entities[1].Name.Should().Be("TestEmptyApiEntity");
+            apiService.Entities[1].IsActive.Should().BeFalse();
+            apiService.Entities[1].Structure.Should().BeNull();
+            apiService.Entities[1].Endpoints.Should().BeEmpty();
+
         }
     }
 }
